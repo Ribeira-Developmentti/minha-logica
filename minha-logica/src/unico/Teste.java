746278089a9d5bdf833590fac73f4fe336e5f954
J. Marcio <J. Marcio@Marcio-PC.local>
2018-07-25 23:54:56 -0300

    Commit 25-07-18 às 23:54

---
+++ minha-logica/src/unico/Teste.java
@@ -0,0 +1,75 @@
+package unico;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		String[] comecoCincoNumeros = {"01", "02", "03", "04", "05"}; 
+		String[] restoCincoNumeros = {"06", "07", "08", "09", "10", "11", "12", "13", "14", "15",
+									  "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"};
+		
+		List<String> resto = new ArrayList<>();
+		for(int i=0; i<restoCincoNumeros.length; i++) {
+			resto.add(restoCincoNumeros[i]);
+		}
+		
+		int indice;
+		Random r = new Random();
+		List<String> escolhidos = new ArrayList<>();
+		for (int i = 1; i <= 10; i++) {
+			indice = r.nextInt(resto.size());
+			escolhidos.add(resto.get(indice));
+			resto.remove(indice);
+		}
+		
+		List<String> volante01 = new ArrayList<>();
+		List<String> volante02 = new ArrayList<>();
+		
+		for (String string : escolhidos) {
+			volante01.add(string);
+		}
+		
+		for(int i = 1; i <= 5; i++) {
+			int indEscolhido = r.nextInt(resto.size());
+			String numeroTroca = resto.get(indEscolhido);
+			resto.remove(indEscolhido);
+			escolhidos.remove(indEscolhido);
+			escolhidos.add(numeroTroca);
+		}
+		
+		for (String string : escolhidos) {
+			volante02.add(string);
+		}
+		
+		for (String string : comecoCincoNumeros) {
+			volante01.add(string);
+		}
+		for (String string : comecoCincoNumeros) {
+			volante02.add(string);
+		}
+		
+		
+		
+		Collections.sort(volante01);
+		Collections.sort(volante02);
+		
+		for (String string : volante01) {
+			System.out.print(string + " - ");
+		}
+		System.out.println();
+		for (String string : volante02) {
+			System.out.print(string + " - ");
+		}
+		
+		
+	}
+
+}
+
